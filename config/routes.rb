cbf92635a8fe7f479c2f6e2f8bb3be728bb0c47c
shmesa22 <shmesa23@gmail.com>
2017-03-21 17:36:07 -0500

    Crea la ruta DELETE /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
   patch '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
 end
