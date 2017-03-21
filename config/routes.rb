815a27d2a39a6e65d432358d5d2b3a3412e566aa
shmesa22 <shmesa23@gmail.com>
2017-03-21 16:26:53 -0500

    Crea la ruta PATCH /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
 end
